f71d8d1807855c4dadc030d808e4d9261b70b5f3
Tim Fielder <fiel1139@gmail.com>
2019-01-25 10:43:02 -0700

    Creates initial methods for the Game class.

--- game_of_life_in_c_sharp/Game.cs
+++ game_of_life_in_c_sharp/Game.cs
@@ -3,6 +3,10 @@ namespace game_of_life_in_c_sharp
 {
     public class Game
     {
+        //void public StartGame()
+        //void public PrintMenu()
+        //void public CreateBoard()
+        //void public PrintBoard()
         public Game()
         {
 
