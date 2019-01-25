87c4c9a9c85b0078375deed76d34290f5b35959c
Tim Fielder <fiel1139@gmail.com>
2019-01-25 10:42:45 -0700

    Creates initial methods for the Board.

--- game_of_life_in_c_sharp/Board.cs
+++ game_of_life_in_c_sharp/Board.cs
@@ -3,6 +3,13 @@ namespace game_of_life_in_c_sharp
 {
     public class Board
     {
+        //public BoardSize
+        //public InitializeBoard(int)
+        //public UpdateBoard()
+          //iterate through each cell
+          //check surrounding cells
+          //return true or false
+        //public Display()
         public Board()
         {
 
