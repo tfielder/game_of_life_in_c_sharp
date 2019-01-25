699bde3391bf3ea4a62a5e5a1b3f7ec764fd814f
Tim Fielder <fiel1139@gmail.com>
2019-01-25 10:04:37 -0700

    Adds the Game of Life starter files.

---
+++ game_of_life_in_c_sharp/Game.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace game_of_life_in_c_sharp
+{
+    public class Game
+    {
+        public Game()
+        {
+        }
+    }
+}
---
+++ game_of_life_in_c_sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace game_of_life_in_c_sharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
