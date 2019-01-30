e1414a70acd004f48588ea55f986f1583486cb80
Tim Fielder <fiel1139@gmail.com>
2019-01-30 15:29:58 -0700

    Comments out implementation for starting the program.

--- game_of_life_in_c_sharp/Program.cs
+++ game_of_life_in_c_sharp/Program.cs
@@ -10,15 +10,15 @@ namespace game_of_life_in_c_sharp
             Console.WriteLine("About: The game of life was originally developed by John Conway in 1970.");
             Console.WriteLine("Type the m key to see the menu at anytime.");
             Console.WriteLine("How big would you like the gameboard to be? Choose a number between 3 and 25.");
-            do
-            {
-                var testInteger = Console.ReadLine();
-                //TryParse, testInteger
-                if (testInteger < 2) || (testInteger > 25)
-                {
-                    Console.WriteLine("Try a number between 3 and 25.");
-                }
-            } while (testInteger < 2 || testInteger > 25);
+            //do
+            //{
+            //    var testInteger = Console.ReadLine();
+            //    //TryParse, testInteger
+            //    if (testInteger < 2) || (testInteger > 25)
+            //    {
+            //        Console.WriteLine("Try a number between 3 and 25.");
+            //    }
+            //} while (testInteger < 2 || testInteger > 25);
         }
     }
 }
