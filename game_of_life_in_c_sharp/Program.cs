219d55cb5741851e19d919fc3b5eb0e82484f991
Tim Fielder <fiel1139@gmail.com>
2019-01-30 15:38:23 -0700

    Removes the word about in the instructions.

--- game_of_life_in_c_sharp/Program.cs
+++ game_of_life_in_c_sharp/Program.cs
@@ -7,7 +7,7 @@ namespace game_of_life_in_c_sharp
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Game of Life in C#!");
-            Console.WriteLine("About: The game of life was originally developed by John Conway in 1970.");
+            Console.WriteLine("The game of life was originally developed by John Conway in 1970.");
             Console.WriteLine("Type the m key to see the menu at anytime.");
             Console.WriteLine("How big would you like the gameboard to be? Choose a number between 3 and 25.");
             //do
