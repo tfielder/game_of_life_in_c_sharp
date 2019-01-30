b0bc8cbe75c25eed5d2957ec75dba12bed8bdbe7
Tim Fielder <fiel1139@gmail.com>
2019-01-30 16:01:50 -0700

    Initial implementation of a menu.

--- game_of_life_in_c_sharp/Program.cs
+++ game_of_life_in_c_sharp/Program.cs
@@ -4,11 +4,27 @@ namespace game_of_life_in_c_sharp
 {
     class MainClass
     {
+        public static void PrintMenu()
+        { 
+            Console.WriteLine("Select from the following options:");
+            Console.WriteLine("1 - Start a new game");
+            Console.WriteLine("2 - Exit the game");
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Game of Life in C#!");
             Console.WriteLine("The game of life was originally developed by John Conway in 1970.");
             Console.WriteLine("Type the m key to see the menu at anytime.");
+
+            var userOption = "0";
+
+            while (userOption != "2")
+            {
+                PrintMenu();
+                userOption = Console.ReadLine();
+            }
+
             //Console.WriteLine("How big would you like the gameboard to be? Choose a number between 3 and 25.");
             //do
             //{
