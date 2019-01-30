47c3e4ce43cda812d35ad9555dc154f5821aff01
Tim Fielder <fiel1139@gmail.com>
2019-01-30 15:26:58 -0700

    Adds notes to Program.cs.

--- game_of_life_in_c_sharp/EmptyClass.cs
+++ game_of_life_in_c_sharp/EmptyClass.cs
@@ -3,8 +3,13 @@ namespace game_of_life_in_c_sharp
 {
     public class EmptyClass
     {
-        public EmptyClass()
+        public class ProgramTest()
         {
+            //Test to see if output is printed to the console.
+
+            //Test to verify user input for the menu
+
+            //Test to see if menu is displayed.
         }
     }
 }
--- game_of_life_in_c_sharp/Program.cs
+++ game_of_life_in_c_sharp/Program.cs
@@ -6,7 +6,19 @@ namespace game_of_life_in_c_sharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Game of Life in C#!");
+            Console.WriteLine("About: The game of life was originally developed by John Conway in 1970.");
+            Console.WriteLine("Type the m key to see the menu at anytime.");
+            Console.WriteLine("How big would you like the gameboard to be? Choose a number between 3 and 25.");
+            do
+            {
+                var testInteger = Console.ReadLine();
+                //TryParse, testInteger
+                if (testInteger < 2) || (testInteger > 25)
+                {
+                    Console.WriteLine("Try a number between 3 and 25.");
+                }
+            } while (testInteger < 2 || testInteger > 25);
         }
     }
 }
